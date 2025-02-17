53e7b541e3943b8c24f03fb8fefb562481ddc3e9
= <wellington.gallo@fieldcorp.com.br>
2025-02-17 17:46:54 -0300

    feat: configuracao para conecatar com banco de dados

--- src/main/java/br/com/gallo/gestao_de_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/gallo/gestao_de_vagas/modules/candidate/CandidateEntity.java
@@ -1,16 +1,26 @@
 package br.com.gallo.gestao_de_vagas.modules.candidate;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
+@Entity(name = "candidate")
 public class CandidateEntity {
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
+  
   private String name;
 
   @Pattern(regexp = "\\S+", message = "O campo [username] não deve conter espaço")
@@ -23,4 +33,7 @@ public class CandidateEntity {
   private String password;
   private String description;
   private String curriculum;
+
+  @CreationTimestamp
+  private LocalDateTime createdAt;
 }
