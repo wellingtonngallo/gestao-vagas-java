e985d75a0b95a080ee3a013cc595663f04f68287
= <wellington.gallo@fieldcorp.com.br>
2025-02-17 18:45:08 -0300

    feat: cria use case para empresa

---
+++ src/main/java/br/com/gallo/gestao_de_vagas/modules/company/useCases/CreateCompanyUseCase.java
@@ -0,0 +1,23 @@
+package br.com.gallo.gestao_de_vagas.modules.company.useCases;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.gallo.gestao_de_vagas.exceptions.UserFoundException;
+import br.com.gallo.gestao_de_vagas.modules.company.entities.CompanyEntity;
+import br.com.gallo.gestao_de_vagas.modules.company.repositories.CompanyRepository;
+
+@Service
+public class CreateCompanyUseCase {
+  @Autowired
+  private CompanyRepository companyRepository;
+
+  public CompanyEntity execute(CompanyEntity companyEntity) {
+    this.companyRepository.findByUsernameOrEmail(companyEntity.getUsername(), companyEntity.getEmail())
+      .ifPresent(company -> {
+        throw new UserFoundException();
+      });
+
+    return this.companyRepository.save(companyEntity);
+  }
+}
