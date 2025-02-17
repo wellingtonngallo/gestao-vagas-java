d604eebaef12967cba71e7b003df82e558db2f0e
= <wellington.gallo@fieldcorp.com.br>
2025-02-17 17:52:31 -0300

    feat: criacao da camada repository

---
+++ src/main/java/br/com/gallo/gestao_de_vagas/modules/candidate/CandidateRepository.java
@@ -0,0 +1,9 @@
+package br.com.gallo.gestao_de_vagas.modules.candidate;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CandidateRepository extends JpaRepository<CandidateEntity, UUID>{
+
+}
--- src/main/java/br/com/gallo/gestao_de_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/gallo/gestao_de_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,19 +1,24 @@
 package br.com.gallo.gestao_de_vagas.modules.candidate.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.gallo.gestao_de_vagas.modules.candidate.CandidateEntity;
+import br.com.gallo.gestao_de_vagas.modules.candidate.CandidateRepository;
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/candidate")
 public class CandidateController {
 
+  @Autowired
+  private CandidateRepository candidateRepository;
+
   @PostMapping("/")
-  public void create(@Valid @RequestBody CandidateEntity candidateEntity) {
-    System.out.println(candidateEntity.getEmail());
+  public CandidateEntity create(@Valid @RequestBody CandidateEntity candidateEntity) {
+    return this.candidateRepository.save(candidateEntity);
   }
 }
